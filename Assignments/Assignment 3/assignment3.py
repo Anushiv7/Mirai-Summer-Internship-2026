bc133d053ba6334c8bfc78ddb3ca49fd7c6f8c74
Anushiv7 <prakashanushiv@gmail.com>
2026-07-13 19:30:22 +0530

    Updating Assignment3.

--- Assignments/Assignment 3/assignment3.py
+++ Assignments/Assignment 3/assignment3.py
@@ -65,8 +65,9 @@ def clear_chat_history():
     st.session_state.messages = []
 
 #sidebar
-personality=st.sidebar.selectbox("Who do you want to talk to? (click to see dropdown)",["Tenz (Valorant Esports player)","Boaster(Valorant Esports player)","Nats(Valorant Esports player)","The Goat(Literally 'Legendary')","Jaun Elia(Famouse Urdu poet)","Muhammad Ali(Famous Boxer)","Ramdhari Singh Dinkar(Famous hindi poet)"],
-                                 on_change=clear_chat_history)
+personality=st.sidebar.selectbox("Who do you want to talk to? (click to see dropdown)",["Tenz (Valorant Esports player)","Boaster(Valorant Esports player)","Nats(Valorant Esports player)","The Goat(Literally 'Legendary')","Jaun Elia(Famouse Urdu poet)","Muhammad Ali(Famous Boxer)","Ramdhari Singh Dinkar(Famous hindi poet)"]
+                                 #,on_change=clear_chat_history
+                                 )
 intensity=st.sidebar.slider("Select the intensity of the response (1-10)", min_value=1, max_value=10, value=5, step=1)
 
 from google import genai
@@ -74,17 +75,16 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 client=genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-if "messages" not in st.session_state:
+if "messages" not in st.session_state: #Task 1: Initialize the Memory Vault
     st.session_state.messages = []
     
-for message in st.session_state.messages:
+for message in st.session_state.messages: #Task 2: Render the Chat History
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-if user_message := st.chat_input("Say something..."):
+if user_message := st.chat_input("Say something..."): #Task 3: Upgrade the Input UI
     with st.chat_message("user"):
         st.write(user_message)
-
         st.session_state.messages.append({"role": "user", "content": user_message})
         with st.spinner("Generating response..."):
             response = client.models.generate_content(
@@ -95,5 +95,6 @@ if user_message := st.chat_input("Say something..."):
             )
         with st.chat_message(personality):
             st.write(response.text)
+            #Task 4: Save New Messages to Memory
             st.session_state.messages.append({"role": "assistant", "content": response.text})
                 
